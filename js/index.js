886634b962eae0b4c84b9c7749e7719c10989d62
FiorellaCrocco <87878994+FiorellaCrocco@users.noreply.github.com>
2021-08-21 23:08:44 -0300

    commit

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-
+/* document.addEventListener("DOMContentLoaded", function(e){
+ */
 });
 
-function login() {
+/* function login() {
     document.getElementById("ingresar").innerHTML = 'bottom';
     setTimeout(()=> location.href="https://fiorellacrocco.github.io/eMercado/home",2200);
-}
+} */
 
 {/* <a href="#" id="texto_mar_movil" onmouseover="mouseOverMar_movil()"><img title="Mar" src="css/images/mar_text_white.png"/></a> */}
+
+function login() {
+    window.location.href="http://www.pakainfo.com";
+  }
