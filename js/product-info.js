ffbdcbd698f70e6de6af12a790cbaf80e2c8cee7
FiorellaCrocco <87878994+FiorellaCrocco@users.noreply.github.com>
2021-09-12 18:14:42 -0300

    Entrega3_p1

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,48 @@
+var product = {};
+
+function showImagesGallery(array){
+
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.length; i++){
+        let imageSrc = array[i];
+
+        htmlContentToAppend += `
+        <div class="col-lg-3 col-md-4 col-6">
+            <div class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" src="` + imageSrc + `" alt="">
+            </div>
+        </div>
+        `
+
+        document.getElementById("productImagesGallery").innerHTML = htmlContentToAppend;
+    }
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
+document.addEventListener("DOMContentLoaded", function(e){ /* Funcion para mostrar la información del json PRODUCT_INFO_URL */
+    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            product = resultObj.data;
+
+            let productNameHTML  = document.getElementById("productName");
+            let productDescriptionHTML = document.getElementById("productDescription");
+            let productPriceHTML = document.getElementById("price");
+            let productCountHTML = document.getElementById("soldCount");
+            let productCriteriaHTML = document.getElementById("category");
+        
+            productNameHTML.innerHTML = product.name;
+            productDescriptionHTML.innerHTML = product.description;
+            productPriceHTML.innerHTML = product.cost +" "+ product.currency;
+            productCountHTML.innerHTML = product.soldCount;
+            productCriteriaHTML.innerHTML = product.category;
+
+            //Muestro las imagenes en forma de galería
+            showImagesGallery(product.images);
+        }
+    });
 });
