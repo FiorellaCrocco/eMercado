81880cdf1ef16668dbc5feb1dbb0fbdf91542791
FiorellaCrocco <87878994+FiorellaCrocco@users.noreply.github.com>
2021-10-26 21:25:27 -0300

    Respaldo

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,6 +1,23 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+
+function guardar(){
+    let usuario = document.getElementById("name").value;
+    let edad = document.getElementById("age").value
+    let email = document.getElementById("email").value
+    let tel = document.getElementById("tel").value
+
+    if(usuario === "" || edad === "" || email === "" || tel === ""){
+        alert("Complete todos los datos");
+    }else{
+        localStorage.setItem('Usuario',JSON.stringify(usuario)); /*agregado*/    
+        localStorage.setItem('Edad',JSON.stringify(edad));
+        localStorage.setItem('Email',JSON.stringify(email));
+        localStorage.setItem('Tel',JSON.stringify(tel));
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function (e) {
 
 });
