ead6fcc023a126b17d52a15dcb8b2f2c1e712a56
FiorellaCrocco <87878994+FiorellaCrocco@users.noreply.github.com>
2021-10-31 01:13:39 -0300

    Update my-profile.js

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,29 +1,4 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
- 
 
-/* const nombreInput = document.getElementById("name")
-const edadInput = document.getElementById("age")
-const CLAVE_USUARIO = username;
-
-function recuperarDatos(key){
-  return localStorage.getItem(key);
-}
-const usuario = document.getElementById("name")
-
-function guardarDatos(){
-  let nombre = nombreInput.value
-  let edad = edad.value
-
-  let usuario = {
-    nombre: nombre,
-    edad: edad
-  }
-  let user = JSON.stringify(usuario);
-  localStorage.setItem(CLAVE_USUARIO,user);
-}
-*/
 const nombreform = document.getElementById("name");
 const edadform = document.getElementById("age");
 const emailform = document.getElementById("email");
@@ -54,10 +29,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
   }
 
 });
-/*  usuario.innerHTML= userform;
- edad.innerHTML=edadform;
- email.innerHTML=emailform;
- tel.innerHTML=telform;  */
+
   
   function guardar(){
     let usuario = document.getElementById("name").value;
@@ -75,52 +47,3 @@ document.addEventListener("DOMContentLoaded", function (e) {
     }
   }  
   
-  /*   let user = recuperarDatos(CLAVE_USUARIO);
-  
-    if(user!=null){
-      user = JSON.parse(user);
-      usuario.value = user.name;
-    } */
-/*
-function guardarDatos(){
-  nombre = nombreform.value;
-  edad = edadform.value;
-  email = emailform.value;
-  tel = telform.value;
-
-  let usuario = {
-    nombre: nombre,
-    edad: edad,
-    email: email,
-    tel: tel
-  }
-  let user = JSON.stringify(usuario);
-  localStorage.setItem(CLAVE_USUARIO.user);
-} */
-
-
-/* function form(){
-let usuario = JSON.parse(localStorage.getItem('Usuario'));
-let userform = JSON.parse(localStorage.getItem('name'));
-let edadform = JSON.parse(localStorage.getItem('age'));
-let emailform = JSON.parse(localStorage.getItem('email'));
-let telform = JSON.parse(localStorage.getItem('tel'));  
-
- if(username !== ""){
-  userform.innerHTML= usuario;
-  edadform.innerHTML=edad;
-  emailform.innerHTML=email;
-  telform.innerHTML=tel; 
- }  */
-
-/*}*/
-
-  /*   let user = recuperarDatos(CLAVE_USUARIO);
-  console.log('user:',user)
-  if (user != null){
-    let user = JSON.parse(user);
-    nombreform.value = user.nombre;
-    edadform.value = user.edad;
-    emailform.value = user.email;
-    telform.value = user.tel
-  } */
